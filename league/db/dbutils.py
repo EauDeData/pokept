4ffc9b1cdd5bb6a5d9c5ff814c24499430798c33
EauDeData <amolina@cvc.uab.cat>
2024-04-01 18:41:58 +0200

    some agents stuff

---
+++ league/db/dbutils.py
@@ -0,0 +1,3 @@
+# People should be able to upload files with strategies
+# Once a strategy is uploaded, main.py will trigger a simutils function to run the simulations
+# In front it will be shown (thanks to flask) the current state of the league
