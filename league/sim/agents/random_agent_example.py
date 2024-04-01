04489b28cbd01d056fe3732fb8779fcec40d8208
EauDeData <amolina@cvc.uab.cat>
2024-04-01 18:40:31 +0200

    pokemon league idea

---
+++ league/sim/agents/random_agent_example.py
@@ -0,0 +1,3 @@
+from poke_env.player import RandomPlayer
+
+# Just importing the random player here so we can use it the same way a user uploaded it.
---
+++ league/sim/simutils.py
@@ -0,0 +1,4 @@
+# Here we will take agents from the agents/ folder and make them battle automatically
+# To determine which is the best agent
+# Users might upload agents there
+# In the database we will have {agent_name: class_name, file_name: file_name}
