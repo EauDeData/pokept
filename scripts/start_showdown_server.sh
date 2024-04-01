28969497180d4e2e1906532e4151275dc06409b2
EauDeData <amolina@cvc.uab.cat>
2024-04-01 16:51:53 +0200

    initial commit

---
+++ scripts/start_showdown_server.sh
@@ -0,0 +1,5 @@
+# Warning! Node has to be installed
+# In version 16 (therefore also install nvm)
+# Also, check that the config file allows bots
+# As documented in https://github.com/hsahovic/poke-env
+node pokemon-showdown start --no-security
